f9d64d42635fd85add391afc26767ef705f6807f
A1iHassan <ali012wkout@gmail.com>
2026-06-28 16:01:51 +0300

    created database migration file

---
+++ backend/migration.sql
@@ -0,0 +1,41 @@
+CREATE TABLE categories ( 
+    id INT PRIMARY KEY, 
+    name TEXT
+);
+
+CREATE TABLE icons (
+    id INT PRIMARY KEY, 
+    name TEXT, 
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE status (
+    id INT PRIMARY KEY, 
+    name TEXT, 
+    color TEXT
+);
+
+CREATE TABLE locations (
+    id INT PRIMARY KEY, 
+    name TEXT, 
+    address TEXT, 
+    created_at DATE, 
+    updated_at DATE
+);
+
+CREATE TABLE assets (
+    id INT PRIMARY KEY, 
+    name TEXT, 
+    count INT, 
+    location_id INT,
+    status_id INT,
+    icon_id INT,
+    category_id INT,
+    created_at DATE, 
+    updated_at DATE,
+    FOREIGN KEY (location_id) REFERENCES locations(id),
+    FOREIGN KEY (status_id) REFERENCES status(id),
+    FOREIGN KEY (icon_id) REFERENCES icons(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
