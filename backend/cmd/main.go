31f1c9c242b12511de9edf2610034b10a27568d7
A1iHassan <ali012wkout@gmail.com>
2026-06-30 15:21:52 +0300

    added CORS to backend (Note: CORS now are set to '*'. change to specific domain on prod deployment)

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -8,12 +8,14 @@ import (
 	"main/internal/services"
 	"net/http"
 	"os"
-
+	"main/internal/middleware"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
 	ctx := context.Background()
+
+	mux := http.NewServeMux()
 
 	pool, err := pgxpool.New(ctx, "postgres://aha:aliPass@localhost:5432/erp")
 	if err != nil {
@@ -32,12 +34,19 @@ func main() {
 	svc := &services.AssetsService{Repo: repo}
 	handler := &handlers.AssetsHandler{Svc: *svc}
 
-	http.HandleFunc("GET /", handler.HandleGetAssets)
-	http.HandleFunc("POST /", handler.HandlePostAssets)
-	http.HandleFunc("PATCH /", handler.HandlePatchAssets)
-	http.HandleFunc("DELETE /{id}", handler.HandleDeleteAssets)
+	mux.HandleFunc("GET /", handler.HandleGetAssets)
+	mux.HandleFunc("POST /", handler.HandlePostAssets)
+	mux.HandleFunc("PATCH /", handler.HandlePatchAssets)
+	mux.HandleFunc("DELETE /{id}", handler.HandleDeleteAssets)
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	corsHandler := middleware.CORS(mux)
+
+	srv := &http.Server{
+		Addr: ":8080",
+		Handler: corsHandler,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
 		fmt.Printf("Couldn't start server due to error: %v", err)
 		os.Exit(1)
 	}
