e19d3e7cae1eb3b8f8cc6f6f3436bf51b2e32a45
A1iHassan <ali012wkout@gmail.com>
2026-07-08 16:10:09 +0300

    implemented a protected routes component on all routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Protected from "./components/protected";
 import AppLayout from "./layouts/AppLayout";
 import AssetsPage from "./pages/AssetsPage";
 import UsersPage from "./pages/UsersPage";
@@ -12,13 +13,16 @@ export default function App() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
-        <Route path="/" element={<Navigate to="/assets" replace />} />
-        <Route path="/assets" element={<AssetsPage />} />
-        <Route path="/sales" element={<Sales />} />
-        <Route path="/users" element={<UsersPage />} />
-        <Route path="/statistics" element={<StatisticsPage />} />
-        <Route path="/customization" element={<CustomizationPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/auth" element={<Auth />} />
+	<Route element={<Protected />}>
+          <Route path="/" element={<Navigate to="/assets" replace />} />
+          <Route path="/assets" element={<AssetsPage />} />
+          <Route path="/sales" element={<Sales />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/statistics" element={<StatisticsPage />} />
+          <Route path="/customization" element={<CustomizationPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+	</Route>
       </Route>
     </Routes>
   );
---
+++ app/src/components/protected.tsx
@@ -0,0 +1,19 @@
+import { useQuery} from "@tanstack/react-query";
+import axios from "axios";
+import { Navigate, Outlet } from "react-router-dom";
+
+const Protected = () => {
+	const {data, isLoading} = useQuery({
+		queryKey: ['auth'],
+		queryFn: async () => {
+			const response = await axios.get("http://localhost:8080/auth/me", {
+				withCredentials: true,
+			})
+			console.log(response.data)
+		}
+	})
+	if (isLoading) return <div>Checking user data...</div>
+	return data ? <Outlet /> : <Navigate to="/auth" replace />
+}
+
+export default Protected;
