ab82657928a2dbe4610174420c7596a2a405bcde
A1iHassan <ali012wkout@gmail.com>
2026-07-21 15:02:55 +0300

    edit database init file

--- database/init.sql
+++ database/init.sql
@@ -1,15 +1,15 @@
-CREATE TABLE icons (
-    id UUID PRIMARY KEY,
+CREATE TABLE icons ( 
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT UNIQUE
 );
 
 CREATE TABLE assets (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT,
     assets_count INT,
     assets_value INT,
-    created_at TIMESTAMPTZ,
-    updated_at TIMESTAMPTZ,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     icon_id UUID,
     FOREIGN KEY (icon_id) REFERENCES icons(id)
 );
@@ -22,25 +22,25 @@ CREATE TABLE permissions (
 );
 
 CREATE TABLE roles (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT,
-    created_at TIMESTAMPTZ,
-    updated_at TIMESTAMPTZ
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE users (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT,
     email TEXT UNIQUE,
     password TEXT,
-    created_at TIMESTAMPTZ,
-    updated_at TIMESTAMPTZ,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     role_id UUID,
     FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
 CREATE TABLE role_permission (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     role_id UUID,
     permission_id UUID,
     FOREIGN KEY (role_id) REFERENCES roles(id),
