f33975c0c88be059c3cc0d00c02e85b09dc34c9a
A1iHassan <ali012wkout@gmail.com>
2026-08-01 16:52:50 +0300

    added submission button and prompt to log in or sign up

--- frontend/src/pages/authPage.tsx
+++ frontend/src/pages/authPage.tsx
@@ -10,6 +10,13 @@ const AuthPage = () => {
 		  <input className="outline-none rounded-lg border-2 border-solid border-slate-200 focus:border-slate-400 p-2" type="email" name="email" id="email" placeholder="Enter your email" />
 		  <label className="text-2xl font-light" htmlFor="password">Password</label>
 		  <input className="outline-none rounded-lg border-2 border-solid border-slate-200 focus:border-slate-400 p-2" type="password" name="password" id="password" placeholder="Enter your password" />
+		  <button 
+		  type="submit"
+		  className="px-5 py-2 rounded-lg border border-slate-200 hover:border-slate-600 hover:bg-slate-600 hover:text-white
+			     transition-color duration-200 cursor-pointer self-center mt-10">{"button"}</button>
+		  <span className="text-slate-600 font-light">
+		    this is where users chose to Log in or Sign up
+		  </span>
 		</Form>
 		</div>
 	)
