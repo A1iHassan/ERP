8e120ef5fdf802374622a5bdc465261b3aba61b2
A1iHassan <ali012wkout@gmail.com>
2026-08-01 16:37:07 +0300

    styled log in/sign up form

--- frontend/src/pages/authPage.tsx
+++ frontend/src/pages/authPage.tsx
@@ -2,14 +2,16 @@ import { Form } from "react-router"
 
 const AuthPage = () => {
 	return (
-		<Form>
-		  <label htmlFor="name">Name</label>
-		  <input type="text" name="name" id="name" placeholder="Enter your name" />
-		  <label htmlFor="email">Email</label>
-		  <input type="email" name="email" id="email" placeholder="Enter your email" />
-		  <label htmlFor="password">Password</label>
-		  <input type="password" name="password" id="password" placeholder="Enter your password" />
+		<div className="flex justify-center items-center h-svh">
+		<Form className="flex flex-col justify-center items-start gap-4 border border-slate-50 shadow-lg p-16 rounded-2xl">
+		  <label className="text-2xl font-light" htmlFor="name">Name</label>
+		  <input className="outline-none rounded-lg border-2 border-solid border-slate-200 focus:border-slate-400 p-2" type="text" name="name" id="name" placeholder="Enter your name" />
+		  <label className="text-2xl font-light" htmlFor="email">Email</label>
+		  <input className="outline-none rounded-lg border-2 border-solid border-slate-200 focus:border-slate-400 p-2" type="email" name="email" id="email" placeholder="Enter your email" />
+		  <label className="text-2xl font-light" htmlFor="password">Password</label>
+		  <input className="outline-none rounded-lg border-2 border-solid border-slate-200 focus:border-slate-400 p-2" type="password" name="password" id="password" placeholder="Enter your password" />
 		</Form>
+		</div>
 	)
 }
 
