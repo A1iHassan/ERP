95a583027934794693486e8323381546cc98c35f
A1iHassan <ali012wkout@gmail.com>
2026-08-06 02:52:19 +0300

    added secondary navigation bar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,11 +1,25 @@
+import { Bars2Icon, Bars3Icon } from "@heroicons/react/16/solid";
+
 const NavBar = () => {
-	return <nav className="fixed left-0 h-full bg-red-100">
-	  <ul className="flex flex-col gap-4 items-center">
-	    <li>tab 1</li>
-	    <li>tab 2</li>
-	    <li>tab 3</li>
-	    <li>tab 4</li>
-	  </ul>
+	return <nav className="fixed left-0 h-full bg-red-100 flex">
+	  <div>
+	    <Bars2Icon width={28} height={28}/>
+	    <ul className="flex flex-col gap-4 items-center">
+	      <li>tab 1</li>
+	      <li>tab 2</li>
+	      <li>tab 3</li>
+	      <li>tab 4</li>
+	    </ul>
+	  </div>
+	  <div>
+	    <Bars3Icon width={28} height={28}/>
+	    <ul className="flex flex-col gap-4 items-center">
+	      <li>tab 1</li>
+	      <li>tab 2</li>
+	      <li>tab 3</li>
+	      <li>tab 4</li>
+	    </ul>
+	  </div>
 	</nav>
 }
 
