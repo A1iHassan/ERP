f3b262adbe57dfd2f23c7da2ff358c4a808bedeb
A1iHassan <ali012wkout@gmail.com>
2026-08-17 00:49:50 +0300

    restructuring nav bar. got to go. not finished

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,23 +1,36 @@
 import { Bars2Icon, Bars3Icon } from "@heroicons/react/16/solid";
+import { Link } from "react-router";
 
 const NavBar = () => {
+	const mainTabs = [
+		{name: "Users", path: "/users"},
+		{name: "Accounting", path: "/accounting"},
+		{name: "Inventory", path: "/inventory"},
+	]
+	const subTabs = [
+		{name: "", path: ""},
+		{name: "", path: ""},
+		{name: "", path: ""},
+	] 
 	return <nav className="fixed left-0 h-full bg-red-100 flex">
 	  <div>
 	    <Bars2Icon width={28} height={28}/>
 	    <ul className="flex flex-col gap-4 items-center">
-	      <li>tab 1</li>
-	      <li>tab 2</li>
-	      <li>tab 3</li>
-	      <li>tab 4</li>
 	    </ul>
+	    {
+		    mainTabs.map((tab, index) => (
+			    <li key={index} className=""><Link to={{pathname: tab.path}}>{tab.name}</Link></li>
+		    ))
+	    }
 	  </div>
 	  <div>
 	    <Bars3Icon width={28} height={28}/>
 	    <ul className="flex flex-col gap-4 items-center">
-	      <li>tab 1</li>
-	      <li>tab 2</li>
-	      <li>tab 3</li>
-	      <li>tab 4</li>
+	    {
+		    subTabs.map((tab, index) => (
+			    <li key={index} className=""><Link to={{pathname: tab.path}}>{tab.name}</Link></li>
+		    ))
+	    }
 	    </ul>
 	  </div>
 	</nav>
